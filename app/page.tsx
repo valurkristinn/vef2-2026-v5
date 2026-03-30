761b58014a6111e64e0d03fd9f28f54fb846fa91
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-03-30 13:53:55 +0000

    added sanity

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
+import Link from "next/link";
+import { type SanityDocument } from "next-sanity";
 
-export default function Home() {
-  return <div>hello</div>;
+import { client } from "../src/sanity/client";
+
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function IndexPage() {
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post._id}>
+            <Link href={`/${post.slug.current}`}>
+              <h2>{post.title}</h2>
+              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
